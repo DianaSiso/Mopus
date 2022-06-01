1db06514d72d6fce05e2478635b0241352c9c78e
Diana Siso <diana.siso@ua.pt>
2022-06-01 15:54:11 +0100

    add button sign out and fix layout problems

---
+++ Mopus/app/src/main/java/com/example/mopus/ui/stats/StatsFragment.java
@@ -0,0 +1,35 @@
+package com.example.mopus.ui.stats;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.mopus.databinding.FragmentStatsBinding;
+
+public class StatsFragment extends Fragment {
+
+    private FragmentStatsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        StatsViewModel statsViewModel =
+                new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(StatsViewModel.class);
+
+        binding = FragmentStatsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ Mopus/app/src/main/java/com/example/mopus/ui/stats/StatsViewModel.java
@@ -0,0 +1,20 @@
+package com.example.mopus.ui.stats;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class StatsViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public StatsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Profile fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
+
