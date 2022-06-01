399516d119b9e1077ea0ff746976e1fae5d6bda0
Diana Siso <diana.siso@ua.pt>
2022-06-01 15:28:01 +0100

    profile
    edit birth date

--- Mopus/app/src/main/java/com/example/mopus/ChangeInfo.java
+++ Mopus/app/src/main/java/com/example/mopus/ChangeInfo.java
@@ -2,9 +2,14 @@ package com.example.mopus;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.InputType;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class ChangeInfo extends AppCompatActivity {
 
@@ -15,7 +20,7 @@ public class ChangeInfo extends AppCompatActivity {
 
         String from = getIntent().getExtras().getString("From");
         Log.d("check", from);
-        TextView info = (TextView) findViewById(R.id.change_info_info);
+        EditText info = (EditText) findViewById(R.id.change_info_info);
 
         switch (from) {
             case "first_name":
@@ -32,7 +37,74 @@ public class ChangeInfo extends AppCompatActivity {
                 info.setHint("E-mail");
                 info.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
                 break;
+            case "birth_date":
+                info.setHint("Birth Date");
+                info.setInputType(InputType.TYPE_CLASS_DATETIME);
 
+                info.addTextChangedListener(new TextWatcher() {
+                    private String current = "";
+                    private String ddmmyyyy = "DDMMYYYY";
+                    private Calendar cal = Calendar.getInstance();
+
+                    @Override
+                    public void onTextChanged(CharSequence seq, int start, int before, int count) {
+                        if (!seq.toString().equals(current)) {
+                            String clean = seq.toString().replaceAll("[^\\d.]", "");
+                            String cleanC = current.replaceAll("[^\\d.]", "");
+
+                            int cl = clean.length();
+                            int sel = cl;
+                            for (int i = 2; i <= cl && i < 6; i += 2) {
+                                sel++;
+                            }
+                            //Fix for pressing delete next to a forward slash
+                            if (clean.equals(cleanC)) sel--;
+
+                            if (clean.length() < 8){
+                                clean = clean + ddmmyyyy.substring(clean.length());
+                            }else{
+                                //This part makes sure that when we finish entering numbers
+                                //the date is correct, fixing it otherwise
+                                int day  = Integer.parseInt(clean.substring(0,2));
+                                int mon  = Integer.parseInt(clean.substring(2,4));
+                                int year = Integer.parseInt(clean.substring(4,8));
+
+                                if(mon > 12) mon = 12;
+                                cal.set(Calendar.MONTH, mon-1);
+
+                                year = (year<1900)?1900:(year>2100)?2100:year;
+                                cal.set(Calendar.YEAR, year);
+                                // ^ first set year for the line below to work correctly
+                                //with leap years - otherwise, date e.g. 29/02/2012
+                                //would be automatically corrected to 28/02/2012
+
+                                day = (day > cal.getActualMaximum(Calendar.DATE))? cal.getActualMaximum(Calendar.DATE):day;
+                                clean = String.format("%02d%02d%02d",day, mon, year);
+                            }
+
+                            clean = String.format("%s/%s/%s", clean.substring(0, 2),
+                                    clean.substring(2, 4),
+                                    clean.substring(4, 8));
+
+                            sel = sel < 0 ? 0 : sel;
+                            current = clean;
+                            info.setText(current);
+                            info.setSelection(sel < current.length() ? sel : current.length());
+
+                        }
+                    }
+
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+                    @Override
+                    public void afterTextChanged(Editable s) {}
+
+                });
+                break;
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + from);
         }
     }
 
