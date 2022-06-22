dd74517b4344f3a4db9acf7126f673cc102560ec
Diana Siso <diana.siso@ua.pt>
2022-06-22 13:34:25 +0100

    .

---
+++ Mopus/app/src/main/java/com/example/mopus/AlarmSleepReceiver.java
@@ -0,0 +1,25 @@
+package com.example.mopus;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class AlarmSleepReceiver  extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "notify")
+                .setSmallIcon(R.drawable.ic_baseline_add_alert_24)
+                .setContentTitle("Mopus Reminder")
+                .setContentText("Time to Sleep!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        notificationManager.notify(200, builder.build());
+
+    }
+}
